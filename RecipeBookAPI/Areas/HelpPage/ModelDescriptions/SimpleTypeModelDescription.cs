762beed0abd9fcbc058fb1d52f8c72117e1710dc
Nikolay Kozlov <Kolgute@mail.ru>
2019-05-18 23:36:07 +0300

    18.05.2019_22:34

---
+++ RecipeBookAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RecipeBookAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RecipeBookAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecipeBookAPI.WebApiApplication" Language="C#" %>
